01ea83e171ab88b2d40fe02b8072b19b286c9e7c
Saitelz <saitelz@yandex.ru>
2022-05-25 19:15:12 +0300

    2022.04.24-Homework-2 upload

--- 2022.04.24-Homework-2/ArrayList/ArrayListLib.h
+++ 2022.04.24-Homework-2/ArrayList/ArrayListLib.h
@@ -1,8 +1,38 @@
-//
-// Created by Pavel Maksimov on 24.04.2022.
-//
+#pragma once
+#include<ostream>
 
-#ifndef ARRAYLIST_ARRAYLISTLIB_H
-#define ARRAYLIST_ARRAYLISTLIB_H
+class ArrayList
+{
+private:
+    int* data;
+    int capacity;
+    int count;
+    void expand(int count = 1);
+    void swap(int posi, int posj);
+    int ind(int index);
 
-#endif //ARRAYLIST_ARRAYLISTLIB_H
+public:
+    ArrayList(int capacity = 10);
+    ArrayList(const ArrayList& list);
+    ~ArrayList();
+
+
+    int size();
+
+    void pushend(int element);
+    void pushbegin(int element);
+
+    void insert(int element, int position);
+
+    int extractbegin();
+    int extractend();
+
+    int extract(int position);
+
+    int& operator[](int pos);
+
+    void sort();
+
+    friend std::ostream& operator<<(std::ostream& stream, ArrayList& list);
+};
+
